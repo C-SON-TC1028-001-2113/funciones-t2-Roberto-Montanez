ce680fb2a7c1a23338f69a4814585e0cff5f08eb
Roberto-Montanez <a01253189@itesm.mx>
2021-08-27 23:03:45 +0000

    Tarea 3 02CalculaGrado

--- assignments/02CalculaGrado/src/exercise.py
+++ assignments/02CalculaGrado/src/exercise.py
@@ -1,3 +1,17 @@
+def calcula_grado(numero): 
+    if numero<1 and numero>0:
+        if numero>=0.9 and numero<1 :
+            return('A')
+        elif numero>=0.8 and numero<0.9 :
+            return('B')
+        elif numero>=0.7 and numero<0.8 :
+            return('C')
+        elif  numero>=0.6 and numero<0.7 :
+            return('D')
+        else: 
+            return('F')
+    else:
+        return('score incorrecto')
 def main():
     #escribe tu código abajo de esta línea
     x = float(input("Ingresa Un valor entre 0.0 y 1.0: "))
